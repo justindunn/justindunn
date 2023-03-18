299926f060f6be5e896655f5068c7d11bdaa795b
Justin Dunn <axeouse@gmail.com>
2023-03-17 20:07:11 -0400

    Adding devise and stuff

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.2/lib/index.js"
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.3.0/js/fontawesome.js"
+pin "@sherifmagdy/animejs", to: "https://ga.jspm.io/npm:@sherifmagdy/animejs@3.3.2/lib/anime.esm.js"
