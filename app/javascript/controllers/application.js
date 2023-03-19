3aad37af01192ed2c1b36173998d85e9aed976b2
Justin Dunn <axeouse@gmail.com>
2023-03-18 20:58:09 -0400

    New Login

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -45,6 +45,23 @@ document.querySelector('#user_password').addEventListener('focus', function(e) {
   });
 });
 
+// document.querySelector('#user_password_confirmation').addEventListener('focus', function(e) {
+//   if (current) current.pause();
+//   current = anime({
+//     targets: 'path',
+//     strokeDashoffset: {
+//       value: -336,
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     },
+//     strokeDasharray: {
+//       value: '240 1386',
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     }
+//   });
+// });
+
 document.querySelector('#submit').addEventListener('focus', function(e) {
   if (current) current.pause();
   current = anime({
